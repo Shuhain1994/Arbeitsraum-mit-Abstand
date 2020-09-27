ce19c9c3b9fb411c47a2d5dcc61077a8d5c7c035
Shuhain1994 <70395404+Shuhain1994@users.noreply.github.com>
2020-09-27 15:51:29 +0200

    27,09,2020

--- MyProblem.py
+++ MyProblem.py
@@ -5,27 +5,29 @@ import geatpy as ea
 import math
 
 """
-    Ich definiere den Arbeitsraum des 2-achsigen-Roboters als f, x1, x2 als 2 Achsen.
-    max f=1/12*math.pi*x2*x1**2
+    Arbeitsraum =4/3*math.pi*(x1**3+x2**3)
+    Position Anlage= np.linalg.norm(p1 - p2)
     s.t.
-    x1 + x2 = 10
+    x1 + x2 <= 14
     2 <= x1 <= 8
-    1 <= x2 <= 6
+    4 <= x2 <= 9
+    2 <= x3, x4 <= 18
+    x1 + x2 - 2 <= sqrt (p1**2 + p2**2) <= x1 + x2 + 5 
 """
 
 
 class MyProblem ( ea.Problem ):
-    def __init__(self):
+    def __init__(self, M=2):
         name = 'MyProblem'
-        M = 1  # Initialize M (target dimension)
-        maxormins = [0]  # Initialize maxormins ( 1: minimize the target; -1: maximize the target)
-        Dim = 2  # Initialize Dim (dimension of decision variable)
+        M = 2  # Initialize M (target dimension)
+        maxormins = [0, 0]  # Initialize maxormins ( 1: minimize the target; -1: maximize the target)
+        Dim = 4  # Initialize Dim (dimension of decision variable)
         varTypes = [0] * Dim
         # Initialize varTypes (types of decision variables, element 0 means continuous; 1 means discrete)
-        lb = [2, 1]  # Lower bound of decision variables
-        ub = [8, 6]  # Upper bound of decision variable
-        lbin = [1, 1]  # 0 means not including the lower boundary of the variable, 1 means including
-        ubin = [1, 1]  # 0 means not including the upper boundary of the variable, 1 means including
+        lb = [2, 4, 2, 2]  # Lower bound of decision variables
+        ub = [8, 9, 18, 18]  # Upper bound of decision variable
+        lbin = [1, 1, 1, 1]  # 0 means not including the lower boundary of the variable, 1 means including
+        ubin = [1, 1, 1, 1]  # 0 means not including the upper boundary of the variable, 1 means including
         # Call the parent class constructor to complete the instantiation
         ea.Problem.__init__ ( self, name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin )
 
@@ -33,7 +35,13 @@ class MyProblem ( ea.Problem ):
         Vars = pop.Phen  # Get the decision variable matrix
         x1 = Vars[:, [0]]
         x2 = Vars[:, [1]]
-        pop.ObjV = 1 / 12 * math.pi * x2 * x1 ** 2
+        x3 = Vars[:, [2]]
+        x4 = Vars[:, [3]]
+        p1 = np.array ( [x3, 2.5], dtype=object)
+        p2 = np.array ( [x4, 13.5], dtype=object)
+        f2 = np.linalg.norm(p1 - p2)
+        f1 = 2 * math.pi * (x1 ** 3 + x2 ** 3)
+        pop.ObjV = np.hstack ( [f1, f2] )
         # Calculate the objective function value and assign it to the ObjV attribute of the pop population object
         # Processing constraints
-        pop.CV = np.hstack ( [np.abs ( x1 + x2 - 10 )] )
+        pop.CV = np.hstack ( [ x1 + x2 - 14, np.linalg.norm(p1 - p2)- x1 - x2 - 5, x1 + x2 - 2 - np.linalg.norm(p1 - p2)] )
--- main.py
+++ main.py
@@ -2,31 +2,44 @@
 import numpy as np
 import geatpy as ea
 from MyProblem import MyProblem
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
-
     problem = MyProblem()
-
     Encoding = 'RI'       # Encoding
-    NIND = 100            # Population size
+    NIND = 20           # Population size
     Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders) # Create a region descriptor
     population = ea.Population(Encoding, Field, NIND) # Instantiate population object
     """================================Algorithm parameter settings============================="""
-    myAlgorithm = ea.soea_DE_rand_1_bin_templet(problem, population) # Instantiate an algorithm template object
-    myAlgorithm.MAXGEN = 200 # Maximum number of evolutions
+    myAlgorithm = ea.moea_NSGA3_DE_templet(problem, population) # Instantiate an algorithm template object
+    myAlgorithm.MAXGEN = 25 # Maximum number of evolutions
     myAlgorithm.mutOper.F = 0.5 # The parameter F in differential evolution
     myAlgorithm.recOper.XOVR = 0.7 # Reorganization Probability
+    myAlgorithm.drawing = 0
     """===========================Call algorithm template for population evolution======================="""
-    [population, obj_trace, var_trace] = myAlgorithm.run() # Execute algorithm template
-    population.save() # Save the information of the last generation population to a file
-    # Output result
-    best_gen = np.argmin(problem.maxormins * obj_trace[:, 1]) # Record the generation of the best population individual
-    best_ObjV = obj_trace[best_gen, 1]
-    print('Der optimale Zielfunktionswert ist：%s'%(best_ObjV))
-    print('Der optimale Wert für die Variable ist：')
-    for i in range(var_trace.shape[1]):
-        print(var_trace[best_gen, i])
-    print('Anzahl der effektiven Evolutionen：%s'%(obj_trace.shape[0]))
-    print('Die beste Generation ist die %s Generation'%(best_gen + 1))
-    print('Anzahl der Bewertungen：%s'%(myAlgorithm.evalsNum))
-    print('Zeitaufwand %s sek'%(myAlgorithm.passTime))
+    NDSet = myAlgorithm.run ()  #
+    NDSet.save ()  #
+    #drawing
+    print ( 'Seconds: %s' % (myAlgorithm.passTime) )
+    print ( 'Non-dominated individuals：%s ' % (NDSet.sizes) )
+    print ( 'Decision variable value：%s ' % (NDSet.Phen) )
+    print ( 'Decision target value：%s ' % (NDSet.ObjV) )
+
+    ax1 = plt.Circle ( (NDSet.Phen[-1,2], 2.5), NDSet.Phen[-1,0], fill=False )
+    ax2 = plt.Rectangle ( (1, 2), 18, 1, fill=False )
+    ax3 = plt.Circle ( (NDSet.Phen[-1,3], 13.5), NDSet.Phen[-1,1], fill=False )
+    ax4 = plt.Rectangle ( (1, 13), 18, 1, fill=False )
+    fig, ax = plt.subplots ()
+
+    ax.add_artist ( ax1 )
+    ax.add_artist ( ax2 )
+    ax.add_artist ( ax3 )
+    ax.add_artist ( ax4 )
+    plt.title ( 'Arbeitsraum mit Abstand (2 Variablen)', fontsize=14 )
+    plt.xlim ( 0, 20 )
+    plt.ylim ( 0, 17 )
+    ax.set_aspect ( 1 )
+
+    plt.show ()
+
